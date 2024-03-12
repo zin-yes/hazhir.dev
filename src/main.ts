f5942ac03d35c7d377225995d7963bb8bbd98531
zin <juztz1n@gmail.com>
2024-03-13 00:43:38 +0100

    Simple 'window'/pane functionality

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ const DEFAULT_BACKGROUND_COLOR = "#2188ad";
 const canvas = document.querySelector("canvas");
 const context = canvas?.getContext("2d");
 
-//const main_element = document.getElementsByTagName("main")[0];
+const main_element = document.getElementsByTagName("main")[0];
 
 let mouse = {
   x: 0,
@@ -30,6 +30,43 @@ function clear_background(context: CanvasRenderingContext2D) {
   context.fillRect(0, 0, window.innerWidth, window.innerHeight);
 }
 
+class Pane {
+  element: HTMLDivElement | null;
+  x: number = 0;
+  y: number = 0;
+  width: number;
+  height: number;
+
+  constructor(width: number, height: number) {
+    this.width = width;
+    this.height = height;
+    this.element = document.createElement("div");
+    this.element.className = "pane";
+
+    this.x = window.innerWidth / 2 - this.width / 2;
+    this.y = window.innerHeight / 2 - this.height / 2;
+
+    this.element.style.position = "absolute";
+    this.element.style.left = `${this.x}px`;
+    this.element.style.top = `${this.y}px`;
+    this.element.style.width = `${this.width}px`;
+    this.element.style.height = `${this.height}px`;
+
+    main_element.appendChild(this.element);
+  }
+
+  update() {
+    if (this.element) {
+      this.element.style.left = `${this.x}px`;
+      this.element.style.top = `${this.y}px`;
+    }
+  }
+
+  draw(context: CanvasRenderingContext2D) {}
+}
+
+let panes: Pane[] = [];
+
 class Icon {
   element: HTMLDivElement | null;
   x: number = 0;
@@ -51,7 +88,7 @@ class Icon {
     this.element.appendChild(name_element);
 
     this.element?.addEventListener("click", () => {
-      alert("folder");
+      panes.push(new Pane(800, 600));
     });
 
     document.getElementById("icon-container")?.appendChild(this.element);
@@ -73,12 +110,18 @@ function update() {
   icons.forEach((icon) => {
     icon.update();
   });
+  panes.forEach((pane) => {
+    pane.update();
+  });
 }
 
 function draw(context: CanvasRenderingContext2D) {
   clear_background(context);
   icons.forEach((icon) => {
     icon.draw(context);
+  });
+  panes.forEach((pane) => {
+    pane.draw(context);
   });
 }
 
